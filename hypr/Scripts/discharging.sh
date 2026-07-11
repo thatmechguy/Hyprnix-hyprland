663d8229277f40db12d43fae9bc591a717f701d2
thatmechguy <thatmechguy12@gmail.com>
2026-07-11 18:54:00 +0530

    added notification

--- hypr/Scripts/discharging.sh
+++ hypr/Scripts/discharging.sh
@@ -5,3 +5,4 @@ hyprctl eval 'hl.config({ decoration = { shadow = { enabled = 0 } } })'
 hyprctl eval 'hl.config({ decoration = { active_opacity = 1.0 } })'
 hyprctl eval 'hl.config({ decoration = { inactive_opacity = 1.0 } })'
 powerprofilesctl set power-saver
+notify-send "Electricity gone gone gone gone gone gone!!!!!!!!"
